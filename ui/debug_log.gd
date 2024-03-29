d311e5aa4831b22ade17d1245c3213a1c23866c6
Joshua Lindhardt <zaphodb2002@gmail.com>
2024-03-29 15:41:10 -0700

    debug log toggle

--- ui/debug_log.gd
+++ ui/debug_log.gd
@@ -1,10 +1,21 @@
-extends Label
+extends VBoxContainer
 class_name DebugLog
 
-@onready var text_edit = get_node("TextEdit")
+@onready var text_edit: TextEdit = get_node("TextEdit")
+
+var open = false
 
 func _ready():
 	write("Log Active")
 
+
 func write(message):
 	text_edit.text += message + "\n"
+
+
+func _on_button_toggled(toggled_on):
+	print(text_edit.size_flags_vertical)
+	if toggled_on:
+		text_edit.size_flags_vertical = Control.SIZE_EXPAND_FILL
+	else:
+		text_edit.size_flags_vertical = Control.SIZE_FILL
