312a87475c19b65652d8e426d95c3a421313e511
Joshua Lindhardt <zaphodb2002@gmail.com>
2024-04-08 12:14:00 -0700

    start game button active when all players are ready

--- ui/lobby.gd
+++ ui/lobby.gd
@@ -1,11 +1,24 @@
 class_name Lobby
 extends Control
 
+@onready var start_game_button: Button = get_node("%StartGameButton")
+
+
 func setup():
+	if Server.is_host():
+		show_start_game_button()
 	for player_panel in get_node("%GridContainer").get_children():
 		player_panel.setup()
 
+func show_start_game_button():
+	start_game_button.show()
+	start_game_button.disabled = not Server.get_ready_status()
+		
 
 func _on_disconnect_button_pressed():
 	Server.disconnect_from_server()
 	UI.set_ui_state(GSGUI.UIState.MainMenu)
+
+
+func _on_start_game_button_pressed():
+	UI.set_ui_state(GSGUI.UIState.World)
