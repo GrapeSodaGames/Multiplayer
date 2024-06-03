83141ada29884e321f3cb94a7ff55f68ea4a5573
Joshua Lindhardt <zaphodb2002@gmail.com>
2024-06-03 11:09:12 -0700

    created PlayerActor scene

---
+++ source/world/actor/player_actor/actor.gd
@@ -0,0 +1,21 @@
+class_name Actor extends CharacterBody2D
+## TODO: Document
+
+# Signals
+
+# Enums
+
+# Exports
+
+# Properties
+
+# References
+
+# Game Loop
+
+# Public Methods
+
+# Private Methods
+
+# Events
+
